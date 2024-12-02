8e1427b70173756545096dba5dedc701715487d7
AgusAdiPranata <agus@ewideplus.com>
2024-12-02 15:49:23 +0700

    Initial Commit

---
+++ ebuddytest/ebuddytestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebuddytestApp.swift
+//  ebuddytest
+//
+//  Created by Ewide Dev 5 on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ebuddytestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ebuddytestTests/ebuddytestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ebuddytestTests.swift
+//  ebuddytestTests
+//
+//  Created by Ewide Dev 5 on 02/12/24.
+//
+
+import Testing
+@testable import ebuddytest
+
+struct ebuddytestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
