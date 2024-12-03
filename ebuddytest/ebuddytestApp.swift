b3a949eb3b0080b078f5380163466ed9cc9c02f2
AgusAdiPranata <agus@ewideplus.com>
2024-12-03 10:11:29 +0700

    [COMMIT-1] Setup firebase including firestore, storage and auth library

--- ebuddytest/ebuddytestApp.swift
+++ ebuddytest/ebuddytestApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct ebuddytestApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
